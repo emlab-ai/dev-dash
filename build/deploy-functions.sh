1460d41f2e51a63dd51f7d1ff798c16f0fdce026
AlexM <malapheev@gmail.com>
2024-03-26 12:07:51 +0000

    added functions

--- build/deploy-functions.sh
+++ build/deploy-functions.sh
@@ -3,7 +3,10 @@
 # delete the existing zip file if it exists
 rm -f functionapp.zip
 
-zip -r functionapp.zip ../server/src -x "*.git*" "*__pycache__*" "*deploy.sh*" ".venv/*"
+cd ../server/src
+zip -r functionapp.zip . -x "*.git*" "*__pycache__*" ".venv/*"
+mv functionapp.zip ../../build/
+cd ../../build
 
 # Replace <your_resource_group>, <your_function_app>, and <your_region> with your values
 az functionapp deployment source config-zip \
@@ -12,5 +15,5 @@ az functionapp deployment source config-zip \
     --src ./functionapp.zip \
     --build-remote true
 
-# az command to create new function python
-# az functionapp create --resource-group DevDashAI_group --consumption-plan-location eastus --name processgithubevent --storage-account devdashai --runtime python --functions-version 3 --os-type Linux
+rm -f functionapp.zip
+
